b8bdfbb6a93d81f6447d0eb97ab66f6f158bdae7
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-14 10:59:10 +0100

    Add a contacts list component

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ListComponent]
 })
 export class ContactsModule { }
--- src/app/contacts/list/list.component.ts
+++ src/app/contacts/list/list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContactsService } from 'src/app/contacts.service';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-list',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  contactsService = inject(ContactsService);
+  contacts: Contact[] = this.contactsService.getAllContacts();
 }

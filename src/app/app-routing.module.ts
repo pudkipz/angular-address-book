8c273ddf0f0cb16dc6da08f3324190b3dea93cb1
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-14 11:24:21 +0100

    Place contacts list page in a router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './contacts/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacts',
+    component: ListComponent,
+  },
+  // {
+  //   path: 'contacts/:id',
+  //   component: 
+  // },
+  // {
+  //   path: 'contacts/new',
+  //   component:
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

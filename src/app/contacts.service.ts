569c18acb0fb0ac1ce756bd8074363899ca92140
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-14 11:16:24 +0100

    Implement contacts service functions

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Contact } from './contacts/contact';
+import { CONTACTS } from './contacts/data/contacts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
+  private contacts: Contact[] = CONTACTS;
+  private currentId: number = this.contacts.length;
 
-  constructor() { }
+  // constructor() { }
+
+  public getAllContacts(): Contact[] {
+    return this.contacts;
+  }
+
+  public getContactById(id: number | null): Contact | null {
+    const contact = this.contacts.find(c => c.id == id);
+    return contact ? contact : null;
+  }
+
+  public addContact(contact: Contact): void {
+    this.contacts.push({...contact, id: this.currentId++})
+  }
 }

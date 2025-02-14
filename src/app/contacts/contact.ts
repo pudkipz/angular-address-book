94ca1c781101563f8154f8b067b78af16a7449b5
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-14 11:06:04 +0100

    Create contact interface and dummy data

--- src/app/contacts/contact.ts
+++ src/app/contacts/contact.ts
@@ -1,2 +1,7 @@
 export interface Contact {
+    id: number,
+    firstName: string,
+    lastName: string,
+    street: string,
+    city: string,
 }
---
+++ src/app/contacts/data/contacts.ts
@@ -0,0 +1,18 @@
+import { Contact } from '../contact';
+
+export const CONTACTS: Contact[] = [
+    {
+        id: 0,
+        firstName: 'Wizard',
+        lastName: 'Wizardson',
+        street: 'Yellow Brick Road',
+        city: 'Oz',
+    },
+    {
+        id: 1,
+        firstName: 'Simon',
+        lastName: 'Garfunkel',
+        street: 'Bleecker Street',
+        city: 'New York',
+    }
+]
